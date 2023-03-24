42bf5127a5e281bb4bff9030f9dfc023b3ffbd3e
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-03-24 09:19:31 +0000

    Deploying to gh-pages from @ HelenBlyzniuk/goit-js-hw-10@26733e50239a8f46bed72d1d24db2046f478ab15 🚀

---
+++ index.3637992b.js
@@ -0,0 +1,2 @@
+fetch(URL).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.3637992b.js.map
---
+++ index.99cc1af5.js
@@ -0,0 +1,2 @@
+fetch(URL).then((o=>{if(!o.ok)throw new Error(o.status);return o.json()})).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
+//# sourceMappingURL=index.99cc1af5.js.map
