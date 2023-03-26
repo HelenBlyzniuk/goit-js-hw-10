a863e2704141310e7e7e1b53deecbdb2c7f7d91e
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-03-26 05:19:09 +0300

    smth has done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,11 @@
-function fetchCountryName(countryName) {
-    const URL = `https://restcountries.com/v3.name/${countryName}?fields=name.official,capital,population,flag.svg,languages`;   
+export default function fetchCountryName(countryName) {
+    const URL = `https://restcountries.com/v3.1/name/${countryName}?fields=name.official,capital,population,flags.svg,languages`;   
    
-        fetch(URL).then(response => {
+       return fetch(URL).then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
     }
             return response.json()
         })
-}
+};
         
-export default  (fetchCountryName) ;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './css/styles.css';
 import fetchCountryName  from './fetchCountries.js';
 import debounce from 'lodash.debounce';
+ 
 
 refs = {
     inputEl: document.querySelector('#search-box'),
+    listEl: document.querySelector('.country-list'),
+    divEl: document.querySelector('.country-info'),
 }
 let debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
@@ -15,11 +18,42 @@ function onCountryInput(e) {
         return
     }
     fetchCountryName(countryName)
-    .then(data => { console.log(data) })
+        // .then(data => {console.log(data)})
+        .then(data => {
+            if (data.length > 10) {
+                alert ("Too many matches found. Please enter a more specific name.")
+            } if(data.length >=2 && data.length <=10) {
+                refs.listEl.innerHTML = markupForSymbols(data);
+            } if (data.length === 1) {
+                refs.listEl.innerHTML = '';
+                console.log(data)
+                refs.divEl.innerHTML = markupForCountry(data); 
+            } 
+           
+        })
     .catch (error =>{ console.log(error)})
     
 }
 
-      
+function markupForSymbols(array) {
+    return array.map(({ capital, name }) => {
+        return `
+            <p>${capital}</p>
+            <p>${ name }</p>`
+       }).join('');
+}  
+   
+function markupForCountry(array) {
+    return array.flatMap(({name, capital, population, flag, languages}) => {
+        return `<p>${name}</p>
+            <p>${capital}</p>
+            <p>${population}</p>
+            <img src="${flag}" alt="flag of ${name}">
+            <p>${ languages }</p>`
+    }).join('');
+             
+}
 
-  
+//   const markup = dataFromLS.map(({ task, id }) => {
+//     return `<li><p> ${task}</p> <button type ="button" data-id ="${id}"> Delete </button> </li> `
+//   }).join('');
